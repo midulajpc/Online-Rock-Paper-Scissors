bf07196e2863081a2f98841f0ba0fbe139c19217
midulajpc <92264558+midulajpc@users.noreply.github.com>
2022-11-11 19:36:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MUHAMMED P C (RA2011033010098)</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Online Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This  starting point for the game
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
